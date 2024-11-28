5f796495204b82556d9fc545eb67c3894662f066
David Williams <davidw1457@gmail.com>
2024-11-28 16:42:57 -0600

    first commit

---
+++ bin/fdarobot.dart
@@ -0,0 +1,5 @@
+import 'package:fdarobot/fdarobot.dart' as fdarobot;
+
+void main(List<String> arguments) {
+  print('Hello world: ${fdarobot.calculate()}!');
+}
---
+++ test/fdarobot_test.dart
@@ -0,0 +1,8 @@
+import 'package:fdarobot/fdarobot.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
